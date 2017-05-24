7f02496c0959848713d9c316aa75bb595194dd9a
rafael.rapbr <rafael.rapbr@dpf.gov.br>
2017-05-24 10:18:26 -0300

    Inferencia

---
+++ inferencia.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  var nome = "Rafael Augusto"
+  // nome = 2 - Aqui vai da erro
+  fmt.Println(nome)
+
+  nomeCompleto := "Go Lang" // Declarando de forma rapida e utilizando inferencia
+  fmt.Println(nomeCompleto)
+}
